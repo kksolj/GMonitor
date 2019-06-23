eef4b2a45aeed81cae5fdd4e6ee08aded16909b3
Zen.Liu <lcz20@163.com>
2019-06-23 23:34:05 +0800

    init

--- src/docker.go
+++ src/docker.go
@@ -3,11 +3,10 @@ package main
 import (
 	"context"
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
-	"log"
+	client2 "github.com/docker/docker/client"
 )
 
-func main() {
+/*func main() {
 	cli, err := client.NewEnvClient()
 	if err != nil {
 		panic(err)
@@ -22,4 +21,50 @@ func main() {
 	for _, container := range containers {
 		log.Printf("%s %s %+v\n", container.ID[0:10], container.Image, container.Names)
 	}
+}*/
+var client *client2.Client
+
+func InitClient() {
+	var err error
+	client, err = client2.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+}
+func tryInitClient() {
+	defer func() {
+		if e := recover(); e != nil {
+			return
+		}
+	}()
+	InitClient()
+}
+func containers(all bool) (c []types.Container) {
+	c, _ = client.ContainerList(context.Background(), types.ContainerListOptions{
+		All: all,
+	})
+	return
+}
+func startContainer(id string) {
+	for _, c := range containers(true) {
+		if c.ID == id {
+			client.ContainerStart(context.Background(), c.ID, types.ContainerStartOptions{})
+		}
+	}
+}
+func startContainerByName(name string) {
+	for _, c := range containers(true) {
+		if sliceContains(c.Names, name) {
+			client.ContainerStart(context.Background(), c.ID, types.ContainerStartOptions{})
+		}
+	}
+}
+
+func sliceContains(str []string, s string) bool {
+	for _, x := range str {
+		if x == s {
+			return true
+		}
+	}
+	return false
 }
